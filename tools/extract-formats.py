4629a46b2fee1cb8e7d166c4f0f3d290ef468f41
Rusty Russell <rusty@rustcorp.com.au>
2017-01-06 11:06:15 +1030

    tools/extract-formats.py: accept fields from BOLT 4.
    Some have no data, and many use constant names and | in their type numbers.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/extract-formats.py
+++ tools/extract-formats.py
@@ -64,15 +64,17 @@ parser.add_option("--message-fields",
 
 (options, args) = parser.parse_args()
 
-# Example input:
-# 1. type: 17 (`MSG_ERROR`)
+# Example inputs:
+# 1. type: 17 (`error`)
 # 2. data:
 #    * [8:channel-id]
 #    * [4:len]
 #    * [len:data]
+#
+# 1. type: PERM|NODE|3 (`required_node_feature_missing`)
 message = None
 havedata = None
-typeline = re.compile('1\. type: (?P<value>[0-9]+) \(`(?P<name>[-A-Za-z_]+)`\)')
+typeline = re.compile('1\. type: (?P<value>[-0-9A-Za-z_|]+) \(`(?P<name>[-A-Za-z_]+)`\)')
 dataline = re.compile('\s+\* \[(?P<size>[-a-z0-9*+]+):(?P<name>[-a-z0-9]+)\]')
 
 for i,line in enumerate(fileinput.input(args)):
@@ -89,7 +91,7 @@ for i,line in enumerate(fileinput.input(args)):
         havedata = None
     elif message is not None and havedata is None:
         if line != '2. data:':
-            raise ValueError('{}:Expected 2. data:'.format(linenum))
+            message = None
         havedata = True
         dataoff = 0
         off_extraterms = ""
