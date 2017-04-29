7d3083e79a9a59bd1c0e977a7ea6d20f5a90e773
ZmnSCPxj via Lightning-dev <lightning-dev@lists.linuxfoundation.org>
2017-04-28 23:19:07 -0400

    Add spellcheck tool (requires `aspell`), also spellcheck.
    This is a multi-part message in MIME format.
    
    This patch should apply to http://github.com/lightningnetwork/lightning-rfc
    
    Nonidealities:
    
    Aspell triggers spelling errors on the hexadecimal strings in
    the test vectors. I don't have enough aspell-fu to figure
    out how to make Aspell ignore these.
    
    There are 2 possible pluralizations of `HTLC`: `HTLCs` and
    `HTLC's`. I'd prefer the latter, but for now I support both.
    We should standardize pluralization; we can edit the
    `.aspell.en.pws` file to remove the pluralization we won't
    choose.

---
+++ tools/spellcheck.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# spellcheck.sh
+# by ZmnSCPxj
+# Simple script to spellcheck files.
+#
+# ZmnSCPxj puts this script into the public domain.
+
+# Check if dirname works.
+if (test_dir=`dirname -- /` && test "X"$test_dir = "X/")
+then
+ my_dirname=dirname
+else
+ my_dirname=false
+fi
+
+# Find the path to this script.
+# We assume sed works on most systems, since it's very old.
+my_dir=`$my_dirname -- "$0" ||
+echo X"$0" |
+ sed '/^X\(.*[^/]\)\/\/*[^/][^/]*\/*$/{
+ s//\1/
+ q
+ }
+ /^X\(\/\/\)[^/].*/{
+ s//\1/
+ q
+ }
+ /^X\(\/\/\)$/{
+ s//\1/
+ q
+ }
+ /^X\(\/\).*/{
+ s//\1/
+ q
+ }
+ s/.*/./; q'`
+
+# This script should be in the tools/ directory of the
+# repository.
+homedir="$my_dir"/..
+
+for f
+do
+ aspell --home-dir ${homedir} -c $f
+done
