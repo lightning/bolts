c7092de08b918dc062293a8dc0086b6d17d0cb92
Rusty Russell <rusty@rustcorp.com.au>
2017-05-02 16:55:02 +0930

    tools/spellcheck.sh: add --check option, add tests to travis.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/spellcheck.sh
+++ tools/spellcheck.sh
@@ -5,6 +5,8 @@
 #
 # ZmnSCPxj puts this script into the public domain.
 
+set -e
+
 # Check if dirname works.
 if (test_dir=`dirname -- /` && test "X"$test_dir = "X/")
 then
@@ -39,7 +41,34 @@ echo X"$0" |
 # repository.
 homedir="$my_dir"/..
 
+if [ x"$1" = x"--check" ]; then
+    CHECK=1
+    shift
+fi
+
 for f
 do
- aspell -l en_US --home-dir ${homedir} -c $f
+    if [ -n "$CHECK" ]; then
+	# Eliminate the following:
+	# #-references eg. [Use of segwit](#use-of-segwit)
+	# quoted identifers eg. `htlc-id`
+	# field descriptions, eg. `* [num-htlcs*64:htlc-signature]'
+	# indented field names, eg. '    num_htlcs: 0'
+	# Short hex strings, eg '0x2bb038521914'
+	# long hex strings
+	# long base58 strings
+	WORDS=$(sed -e 's/\](#[-a-zA-Z0-9_]*)//g' \
+	    -e 's/`[-a-zA-Z0-9_]*`//g' \
+	    -e 's/\* \[[-_a-z0-9*]\+:[-_a-z0-9]\+\]//g' \
+	    -e 's/0x[a-fA-F0-9]\+//g' \
+	    -e 's/[a-fA-F0-9]\{20,\}//g' \
+	    -e 's/^    .*_htlcs//g' \
+	    -e 's/[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]\{20,\}//g' < $f | aspell -l en_US --home-dir ${homedir} list)
+	if [ -n "$WORDS" ]; then
+	    echo Misspelled words in $f: $WORDS >&2
+	    exit 1
+	fi
+    else
+	aspell -l en_US --home-dir ${homedir} -c $f
+    fi
 done
